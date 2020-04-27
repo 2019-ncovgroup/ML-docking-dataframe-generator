bea314c44da865938ab066711e4b4d934edde14e
Alex Partin <25892264+adpartin@users.noreply.github.com>
2020-04-27 16:30:34 -0500

    Update batch file

--- batch.bash
+++ batch.bash
@@ -7,8 +7,8 @@ echo $SCORES_PATH
 echo $FEA_DIR
 # echo $FEA_DIR/BL2.dsc.parquet 
 # python src/main_gen_dfs.py --scores_path $SCORES_PATH --fea_path $FEA_DIR/BL2.dsc.parquet   --par_jobs $JOBS
-python src/main_gen_dfs.py --scores_path $SCORES_PATH --fea_path $FEA_DIR/BL2.ecfp2.parquet --fea_list ecfp2 --outdir ./out/V5_batch/ecfp2 --par_jobs $JOBS
-python src/main_gen_dfs.py --scores_path $SCORES_PATH --fea_path $FEA_DIR/BL2.ecfp4.parquet --fea_list ecfp4 --outdir ./out/V5_batch/ecfp4 --par_jobs $JOBS
-python src/main_gen_dfs.py --scores_path $SCORES_PATH --fea_path $FEA_DIR/BL2.ecfp6.parquet --fea_list ecfp6 --outdir ./out/V5_batch/ecfp6 --par_jobs $JOBS
+python src/main_gen_dfs.py --scores_path $SCORES_PATH --fea_path $FEA_DIR/BL2.ecfp2.parquet --fea_list ecfp2 --outdir ./out/V5_batch_ECFP/ecfp2 --par_jobs $JOBS
+python src/main_gen_dfs.py --scores_path $SCORES_PATH --fea_path $FEA_DIR/BL2.ecfp4.parquet --fea_list ecfp4 --outdir ./out/V5_batch_ECFP/ecfp4 --par_jobs $JOBS
+python src/main_gen_dfs.py --scores_path $SCORES_PATH --fea_path $FEA_DIR/BL2.ecfp6.parquet --fea_list ecfp6 --outdir ./out/V5_batch_ECFP/ecfp6 --par_jobs $JOBS
 
 
