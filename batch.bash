38349d365da9d7ca2fe4b28e8326b208b56147c6
Alex Partin <25892264+adpartin@users.noreply.github.com>
2020-05-08 22:15:04 -0500

    Update batch script

--- batch.bash
+++ batch.bash
@@ -1,10 +1,15 @@
 #!/bin/bash
-SCORES_PATH="data/raw/raw_data/V5_docking_data_april_24/pivot_TITLE.csv"
-FEA_DIR="data/raw/features/BL2"
-# BL_SET="BL2"
-JOBS=4
-echo $SCORES_PATH
-echo $FEA_DIR
+# SCORES_PATH="data/raw/raw_data/V5_docking_data_april_24/pivot_TITLE.csv"
+# FEA_DIR="data/raw/features/BL2"
+# JOBS=32
+
+SCORES_PATH=$1
+FEA_DIR=$2
+JOBS=$3
+
+echo "Scores path $SCORES_PATH"
+echo "Features path $FEA_DIR"
+
 # echo $FEA_DIR/BL2.dsc.parquet 
 # python src/main_gen_dfs.py --scores_path $SCORES_PATH --fea_path $FEA_DIR/BL2.dsc.parquet   --par_jobs $JOBS
 python src/main_gen_dfs.py --scores_path $SCORES_PATH --fea_path $FEA_DIR/BL2.ecfp2.parquet --fea_list ecfp2 --outdir ./out/V5_batch_ECFP/ecfp2 --par_jobs $JOBS
